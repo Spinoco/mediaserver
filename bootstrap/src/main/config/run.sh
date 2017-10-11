1a0a96acfc8c01de68ad61a3f2ec8c94d7355a56
Milan Raulim <milan.raulim@spinoco.com>
2017-10-11 16:05:20 +0200

    5.0.0-rt-sharedQ JAVA_OPTS modification

--- bootstrap/src/main/config/run.sh
+++ bootstrap/src/main/config/run.sh
@@ -133,7 +133,7 @@ if [ "x$SERVER_SET" = "x" ]; then
 fi
 
 # Setup MMS specific properties
-JAVA_OPTS="-Dprogram.name=$PROGNAME -XX:+UseG1GC -XX:MaxGCPauseMillis=40 -XX:+ParallelRefProcEnabled -XX:G1HeapRegionSize=1m  -XX:+UnlockExperimentalVMOptions  -XX:+AlwaysPreTouch -XX:+UseCompressedOops -Djava.net.preferIPv4Stack=true -Dorg.jboss.resolver.warning=true -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000 -Dhttp.keepAlive=false $JAVA_OPTS"
+JAVA_OPTS="-Dprogram.name=$PROGNAME $JAVA_OPTS"
 #JAVA_OPTS="$JAVA_OPTS -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n"
 
 # Setup the java endorsed dirs
