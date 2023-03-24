cf639b7bc6e5e8487560e7b681ad3bfc733c68cc
Adam Chlupacek <adam.chlupacek@spinoco.com>
2023-03-24 11:25:52 +0100

    Fix narrowband decode.

--- codecs/amr/src/main/java/org/restcomm/media/codec/amr/nb/Decoder.java
+++ codecs/amr/src/main/java/org/restcomm/media/codec/amr/nb/Decoder.java
@@ -8,7 +8,11 @@ public class Decoder extends AMRDecoder {
     public final static Format amr = FormatFactory.createAudioFormat("amr", 8000, 8, 1);
     private final static String AMR_CODEC_NAME = "libopencore_amrnb";
     private final static int AMR_SAMPLE_RATE= 8000;
-    public final static int[] payloadSizes = { 12, 13, 15, 17, 19, 20, 26, 31, 5, 0, 0, 0, 0, 0, 0, 0 };
+
+    // Real payload sizes are as bellow, we are compensating here for toc.
+    // This compensation happens only in NB AMR.
+    // { 12, 13, 15, 17, 19, 20, 26, 31, 5, 0, 0, 0, 0, 0, 0, 0 }
+    public final static int[] payloadSizes = { 13, 14, 16, 18, 20, 21, 27, 32, 6, 0, 0, 0, 0, 0, 0, 0 };
 
     public Decoder() {
         super(AMR_CODEC_NAME, AMR_SAMPLE_RATE, payloadSizes);
